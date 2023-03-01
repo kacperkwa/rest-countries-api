3b4aae22e4c96b2f330aab1e96c0d1f0e55d7687
kacperkwa <kacperkwartaxz@gmail.com>
2023-03-01 19:05:09 +0100

    added scripts for shearching single countries by their name

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 const countryContainer = document.querySelector('.country-cards');
 const searchInput = document.querySelector('#search-input');
 const filter = document.querySelector('#filter');
-
 const API_URL = 'data.json';
 let countryName;
 const sendHttpRequest = API_URL => {
@@ -19,14 +18,14 @@ async function fetchCountries() {
 			countryContainer.innerHTML =
 				countryContainer.innerHTML +
 				`<div class="card">
-      <img src="${country.flag}" class="card__img" alt="${country.name} flag">
-      <div class="card__description">
+			<img src="${country.flag}" class="card__img" alt="${country.name} flag">
+			<div class="card__description">
          <h2 class="card__name">${country.name}</h2>
          <p class="card__population"><span>Population:</span> ${country.population}</p>
          <p class="card__region"><span>Region:</span> ${country.region}</p>
          <p class="card__capital"><span>Capital:</span> ${country.capital}</p>
-      </div>
-      </div>`;
+			</div>
+			</div>`;
 		} else if (filter.value === 'all') {
 			countryContainer.innerHTML =
 				countryContainer.innerHTML +
@@ -42,6 +41,40 @@ async function fetchCountries() {
 		}
 	});
 }
+async function fetchByName() {
+	const responseData = await sendHttpRequest(API_URL);
+	const listOfCountries = responseData;
+	const countryName = searchInput.value.toUpperCase().trim();
+	countryContainer.innerHTML = ``;
+	listOfCountries.forEach(country => {
+		if (country.name.toUpperCase() === countryName) {
+			countryContainer.innerHTML =
+				countryContainer.innerHTML +
+				`<div class="card">
+	<img src="${country.flag}" class="card__img" alt="${country.name} flag">
+	<div class="card__description">
+		<h2 class="card__name">${country.name}</h2>
+		<p class="card__population"><span>Population:</span> ${country.population}</p>
+		<p class="card__region"><span>Region:</span> ${country.region}</p>
+		<p class="card__capital"><span>Capital:</span> ${country.capital}</p>
+	</div>
+	</div>`;
+		}
+	});
+}
 
+let cards;
+const modalHandler = e => {
+	cards = document.getElementsByClassName('card');
+	console.log(cards);
+};
 
 filter.addEventListener('change', fetchCountries, false);
+searchInput.addEventListener('keyup', e => {
+	if (e.key === 'Enter') {
+		fetchByName();
+	}
+});
+for (let card of cards) {
+	card.addEventListener('click', modalHandler);
+}
