6ca9fc7cce1cebda25054d43c4fb70466f7aff7c
kacperkwa <kacperkwartaxz@gmail.com>
2023-03-07 18:49:09 +0100

    added modal

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 const countryContainer = document.querySelector('.country-cards');
 const searchInput = document.querySelector('#search-input');
-const filter = document.querySelector('#filter');
+const filter = document.getElementById('filter');
+const wrapper = document.querySelector('.wrapper');
+const backBtn = document.getElementsByClassName('back-btn');
+const modalSection = document.querySelector('.modal');
+
+const main = document.querySelector('.main');
 const API_URL = 'data.json';
 let countryName;
 const sendHttpRequest = API_URL => {
@@ -21,7 +26,7 @@ async function fetchCountries() {
 			<img src="${country.flag}" class="card__img" alt="${country.name} flag">
 			<div class="card__description">
          <h2 class="card__name">${country.name}</h2>
-         <p class="card__population"><span>Population:</span> ${country.population}</p>
+         <p class="card__population"><span>Population:</span> ${country.population.toLocaleString()}</p>
          <p class="card__region"><span>Region:</span> ${country.region}</p>
          <p class="card__capital"><span>Capital:</span> ${country.capital}</p>
 			</div>
@@ -33,7 +38,7 @@ async function fetchCountries() {
    <img src="${country.flag}" class="card__img" alt="${country.name} flag">
    <div class="card__description">
       <h2 class="card__name">${country.name}</h2>
-      <p class="card__population"><span>Population:</span> ${country.population}</p>
+      <p class="card__population"><span>Population:</span> ${country.population.toLocaleString()}</p>
       <p class="card__region"><span>Region:</span> ${country.region}</p>
       <p class="card__capital"><span>Capital:</span> ${country.capital}</p>
    </div>
@@ -54,7 +59,7 @@ async function fetchByName() {
 	<img src="${country.flag}" class="card__img" alt="${country.name} flag">
 	<div class="card__description">
 		<h2 class="card__name">${country.name}</h2>
-		<p class="card__population"><span>Population:</span> ${country.population}</p>
+		<p class="card__population"><span>Population:</span> ${country.population.toLocaleString()}</p>
 		<p class="card__region"><span>Region:</span> ${country.region}</p>
 		<p class="card__capital"><span>Capital:</span> ${country.capital}</p>
 	</div>
@@ -66,9 +71,49 @@ async function fetchByName() {
 async function modalHandler(target) {
 	const responseData = await sendHttpRequest(API_URL);
 	const listOfCountries = responseData;
-	console.log(target.closest('.card__name'));
-	
+	const card = target.closest('.card');
+	const cardName = card.querySelector('.card__name').textContent;
+	modalSection.style.display = 'flex';
+	main.style.display = 'none';
+	listOfCountries.forEach(country => {
+		if (country.name === cardName) {
+			modalSection.innerHTML =
+				modalSection.innerHTML +
+				`
+				<div class="modal__container">
+				<button class="back-btn">Back</button> 
+            <div class="countries-description">
+               <img src="${country.flag}" alt="${country.name} flag">
+               <h2 class="name">${country.name}</h2>
+               <p class="native-name"><span>Native Name:</span> ${
+						country.nativeName
+					}</p>
+               <p class="population"><span>Population:</span> ${country.population.toLocaleString()}</p>
+               <p class="region"><span>Region:</span> ${country.region}</p>
+               <p class="sub-region"><span>Sub Region:</span> ${
+						country.subregion
+					}</p>
+               <p class="capital"><span>Capital:</span> ${country.capital}</p>
+               <p class="domain"><span>Top level Domain:</span> ${
+						country.topLevelDomain
+					}</p>
+               <p class="currencies"><span>Currencies:</span> ${country.currencies.map(
+						el => el.name
+					)}</p>
+               <p class="Languages"><span>Languages:</span> ${country.languages.map(
+						el => el.name
+					)}</p>
+            </div>
+         </div>`;
+		}
+	});
 }
+const closeModal = () => {
+	const modalContainer = modalSection.querySelector('.modal__container');
+	main.style.display = 'block';
+	modalSection.style.display = 'none';
+	modalContainer.remove();
+};
 
 filter.addEventListener('change', fetchCountries, false);
 searchInput.addEventListener('keyup', e => {
@@ -80,5 +125,11 @@ document.addEventListener('click', e => {
 	const target = e.target.closest('.card');
 	if (target) {
 		modalHandler(target);
+	}
+});
+document.addEventListener('click', e => {
+	const target = e.target.closest('.back-btn');
+	if (target) {
+		closeModal(target);
 	}
 });
